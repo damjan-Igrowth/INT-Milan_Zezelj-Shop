49c4bd7376cf3752a2b8ed406fa28a2ae5d19358
damjan-Igrowth <119666343+damjan-Igrowth@users.noreply.github.com>
2024-02-23 10:31:25 +0100

    Revert "Added error and succes alerts"
    This reverts commit a6a5a76b5c19e22bff062d515c087a838ac07df7.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -53,7 +53,17 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                TBRating(rating: 5),
+                TBSection(
+                    title: "Details",
+                    content: TBDetailOverviewCard(
+                      name: product.name,
+                      company: product.company,
+                      description: product.description,
+                      discount: product.discount,
+                      price: product.price,
+                      rating: product.rating,
+                    )),
+                const TBRating(rating: 5),
                 TBGallery.url(
                   images: [
                     "https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png",
@@ -65,23 +75,19 @@ class _HomeScreenState extends State<HomeScreen> {
                     "assets/iphone_image.png",
                   ],
                 ),
-                TBSection(
-                  title: "Details",
-                  content: TBDetailOverviewCard(
-                    name: product.name,
-                    company: product.company,
-                    description: product.description,
-                    discount: product.discount,
-                    price: product.price,
-                    rating: product.rating,
-                  ),
-                ),
-                const TBRating(rating: 5),
                 TBButton(
                   type: TBButtonType.filled,
                   onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const HomeScreen()));
+                    showDialog(
+                        barrierDismissible: false,
+                        context: context,
+                        builder: (context) => TBAlertDialog.error(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              message:
+                                  "Something went wrong while editing product!",
+                            ));
                   },
                   text: "Add Product",
                 ),
